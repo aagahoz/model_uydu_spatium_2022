edd5226d21df4e0df6cd588bbac086727e310a76
Agah Özdemir <72024578+aagahoz@users.noreply.github.com>
2022-06-19 22:27:26 +0300

    Add files via upload
    /*
     *  LoRa Receiver       -
     *  BMP280              +
     *  Raspberry Pi UART   -
     */

---
+++ main_Arduino_Gorev_Yuku/main_Arduino_Gorev_Yuku.ino
@@ -0,0 +1,46 @@
+/*
+ *  LoRa Receiver       -
+ *  BMP280              +
+ *  Raspberry Pi UART   -
+ */
+
+#include <Wire.h>
+#include <Adafruit_BMP280.h>
+
+String bmp_payload = "";
+
+
+void setup()
+{
+  unsigned status;
+  status = bmp.begin(0x76);
+}
+
+void loop() 
+{
+    bmp_payload = bmp_returner();
+    main_payload = bmp_payload + "," ;
+
+    Serial.println(main_payload);
+
+
+    main_payload = "";
+    Serial.flush();
+
+}
+
+String bmp_returner()
+{
+    String bmp_temprature = "";
+    String bmp_pressure = "";
+    String bmp_altitude = "";
+    String payload = "";
+    
+    bmp_temprature = String(bmp.readTemperature());
+    bmp_pressure = String(bmp.readPressure());
+    bmp_altitude = String(bmp.readAltitude(1017.5));
+    payload = bmp_pressure + "," + bmp_temprature + "," + bmp_altitude;
+    
+//    Serial.println(bmp_payload);
+    return payload;
+}
