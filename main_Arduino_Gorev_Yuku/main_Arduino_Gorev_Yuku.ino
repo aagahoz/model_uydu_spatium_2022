d9bc52aea98a13e517162bf93eb1d3ee9142a5aa
Agah Özdemir <aa.ozdemir2018@gtu.edu.tr>
2022-07-03 13:35:20 +0300

    lora eklendi

--- main_Arduino_Gorev_Yuku/main_Arduino_Gorev_Yuku.ino
+++ main_Arduino_Gorev_Yuku/main_Arduino_Gorev_Yuku.ino
@@ -21,15 +21,32 @@
 
 #include <Wire.h>
 #include <Adafruit_BMP280.h>
+#include "LoRa_E32.h"
+#include <SoftwareSerial.h>
 
 Adafruit_BMP280 bmp; 
+SoftwareSerial mySerial(10, 11); // Arduino RX <-- e32 TX, Arduino TX --> e32 RX
+LoRa_E32 e32ttl(&mySerial);
+
+typedef  struct 
+{
+  String tas_latitude;
+  String tas_longtitude;
+  String tas_gps_altitude;
+  String tas_pressure;
+  String tas_altitude;
+} Signal;
+Signal tasiyici_lora;
 
 String bmp_payload = "";
+String lora_payload = "";
 String main_payload = "";
 
 void setup()
 {
   Serial.begin(9600);
+  
+  e32ttl.begin();
   
   unsigned status;
   status = bmp.begin(0x76);
@@ -38,22 +55,39 @@ void setup()
 void loop() 
 {
     bmp_payload = bmp_returner();
-    main_payload = bmp_payload + "," ;
+    lora_payload = lora_returner();
 
+    main_payload = bmp_payload + "," +  lora_payload; 
+    
     Serial.println(main_payload);
-
-
-    main_payload = "";
-
+    
+    main_payload = ",,,,,,";
+    lora_payload = ",,,,";
+    bmp_payload = ",,";
+    
     delay(50);
     Serial.flush();
 }
 
 String bmp_returner()
 {    
-    String payload = "";
+    String payload = ",,";
     payload = String(bmp.readTemperature()) + "," + String(bmp.readPressure()) + "," + String(bmp.readAltitude(1017.5));
     
 //    Serial.println(bmp_payload);
     return payload;
+}
+
+
+String lora_returner()
+{
+    String payload = ",,,,";
+
+    while (e32ttl.available()  > 1) 
+    {
+      ResponseStructContainer rsc = e32ttl.receiveMessage(sizeof(Signal));
+      tasiyici_lora = *(Signal*) rsc.data;
+      rsc.close();
+    }
+    payload = tasiyici_lora.tas_latitude + "," + tasiyici_lora.tas_longtitude + "," + tasiyici_lora.tas_gps_altitude + "," + tasiyici_lora.tas_pressure + "," + tasiyici_lora.tas_altitude;
 }
