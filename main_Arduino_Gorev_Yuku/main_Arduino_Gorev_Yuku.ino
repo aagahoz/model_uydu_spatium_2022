544e67f8c879a013808fde435db2ebb429c6bf7c
Agah Özdemir <aa.ozdemir2018@gtu.edu.tr>
2022-07-03 15:00:54 +0300

    sistemsel olarak bitti

--- main_Arduino_Gorev_Yuku/main_Arduino_Gorev_Yuku.ino
+++ main_Arduino_Gorev_Yuku/main_Arduino_Gorev_Yuku.ino
@@ -1,10 +1,16 @@
 /*
- *  LoRa Receiver       -
- *  BMP280              +
- *  Raspberry Pi UART   -
+ *  LoRa Receiver   +
+ *  BMP280          +
+ *  ESP32 UART      +
 
    LoRa UART Pinout
-    -----
+    Vcc         ->   5V
+    Ground      ->   Ground
+    Tx          ->   11     
+    Rx          ->   10
+    AUX         ->   Ground     
+    M0          ->   Ground
+    M1          ->   Ground
       
    BMP280 PinOut   
     Vcc         ->   3.3V
@@ -13,11 +19,10 @@
     SCL         ->   A5
     
    ESP32 PinOut
-
-    ??
-
- 
- */
+    Ground     ->   Ground
+    Tx         ->   4
+    Rx         ->   3
+*/
 
 #include <Wire.h>
 #include <Adafruit_BMP280.h>
@@ -26,15 +31,16 @@
 
 Adafruit_BMP280 bmp; 
 SoftwareSerial mySerial(10, 11); // Arduino RX <-- e32 TX, Arduino TX --> e32 RX
+SoftwareSerial send_to_esp32(3, 4); // Arduino RX <-- esp32 TX, Arduino TX --> esp32 RX
 LoRa_E32 e32ttl(&mySerial);
 
 typedef  struct 
 {
-  String tas_latitude;
-  String tas_longtitude;
-  String tas_gps_altitude;
-  String tas_pressure;
-  String tas_altitude;
+  float tas_latitude;
+  float tas_longtitude;
+  float tas_gps_altitude;
+  float tas_pressure;
+  float tas_altitude;
 } Signal;
 Signal tasiyici_lora;
 
@@ -45,7 +51,7 @@ String main_payload = "";
 void setup()
 {
   Serial.begin(9600);
-  
+  send_to_esp32.begin(38400);
   e32ttl.begin();
   
   unsigned status;
@@ -57,15 +63,17 @@ void loop()
     bmp_payload = bmp_returner();
     lora_payload = lora_returner();
 
-    main_payload = bmp_payload + "," +  lora_payload; 
     
+    main_payload = bmp_payload + "," + lora_payload; 
+
+    send_to_esp32.println(main_payload);
     Serial.println(main_payload);
     
     main_payload = ",,,,,,";
     lora_payload = ",,,,";
     bmp_payload = ",,";
     
-    delay(50);
+    delay(200);
     Serial.flush();
 }
 
@@ -88,6 +96,8 @@ String lora_returner()
       ResponseStructContainer rsc = e32ttl.receiveMessage(sizeof(Signal));
       tasiyici_lora = *(Signal*) rsc.data;
       rsc.close();
+      Serial.println("succes");
     }
-    payload = tasiyici_lora.tas_latitude + "," + tasiyici_lora.tas_longtitude + "," + tasiyici_lora.tas_gps_altitude + "," + tasiyici_lora.tas_pressure + "," + tasiyici_lora.tas_altitude;
+    payload = String(tasiyici_lora.tas_latitude) + "," + String(tasiyici_lora.tas_longtitude) + "," + String(tasiyici_lora.tas_gps_altitude) + "," + String(tasiyici_lora.tas_pressure) + "," + String(tasiyici_lora.tas_altitude);
+    return payload;
 }
