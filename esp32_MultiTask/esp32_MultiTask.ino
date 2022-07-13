91039c4f5d549f347f8a7379f06458622899f6b7
Agah Özdemir <aa.ozdemir2018@gtu.edu.tr>
2022-07-13 16:50:38 +0300

    SD kart eklendi

--- esp32_MultiTask/esp32_MultiTask.ino
+++ esp32_MultiTask/esp32_MultiTask.ino
@@ -132,6 +132,12 @@ typedef  struct
 } Signal;
 Signal data;
 
+// Libraries for SD card
+#include "FS.h"
+#include "SD.h"
+#include <SPI.h>
+#define SD_CS 5
+
 #include <Servo.h>
 
 Adafruit_BMP280 BMP280;
@@ -206,6 +212,36 @@ void setup() {
   {
     Serial.println("RTC is NOT running!");
   }
+
+// Initialize SD card
+  SD.begin(SD_CS);  
+  if(!SD.begin(SD_CS)) {
+    Serial.println("Card Mount Failed");
+    return;
+  }
+  uint8_t cardType = SD.cardType();
+  if(cardType == CARD_NONE) {
+    Serial.println("No SD card attached");
+    return;
+  }
+  Serial.println("Initializing SD card...");
+  if (!SD.begin(SD_CS)) {
+    Serial.println("ERROR - SD card initialization failed!");
+    return;    // init failed
+  }
+  // If the data.txt file doesn't exist
+  // Create a file on the SD card and write the data labels
+  File file = SD.open("/data.txt");
+  if(!file) {
+    Serial.println("File doens't exist");
+    Serial.println("Creating file...");
+    writeFile(SD, "/data.txt", "DATALAR \r\n");
+  }
+  else {
+    Serial.println("File already exists");  
+  }
+  file.close();
+  
   
   // Now set up two tasks to run independently.
   xTaskCreatePinnedToCore(
@@ -572,10 +608,47 @@ void TaskTelemeryLoggerSdCard(void *pvParameters)
     + "," + p14_gps1Altitude + "," + p15_gps2Altitude
     + "," + p16_gps1Longtitude + "," + p17_gps2Longtitude
     + "," + p18_uyduStatusu + "," + p19_pitch + "," + p20_roll
-    + "," + p21_yaw + "," + p22_donusSayisi + "," + p23_videoAktarimBilgisi;
+    + "," + p21_yaw + "," + p22_donusSayisi + "," + p23_videoAktarimBilgisi + "\r\n";
+    
+    appendFile(SD, "/data.txt", main_payload.c_str());
+        
     Serial.print("main_payload---> ");
     Serial.println(main_payload);
 
     vTaskDelay(800);  
   }
+}
+
+
+void appendFile(fs::FS &fs, const char * path, const char * message) {
+  Serial.printf("Appending to file: %s\n", path);
+
+  File file = fs.open(path, FILE_APPEND);
+  if(!file) {
+    Serial.println("Failed to open file for appending");
+    return;
+  }
+  if(file.print(message)) {
+    Serial.println("Message appended");
+  } else {
+    Serial.println("Append failed");
+  }
+  file.close();
+}
+
+// Write to the SD card (DON'T MODIFY THIS FUNCTION)
+void writeFile(fs::FS &fs, const char * path, const char * message) {
+  Serial.printf("Writing file: %s\n", path);
+
+  File file = fs.open(path, FILE_WRITE);
+  if(!file) {
+    Serial.println("Failed to open file for writing");
+    return;
+  }
+  if(file.print(message)) {
+    Serial.println("File written");
+  } else {
+    Serial.println("Write failed");
+  }
+  file.close();
 }
