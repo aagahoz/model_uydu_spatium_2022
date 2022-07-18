12cdf2a19bdaaad787ee8df212465f6f2ba1438a
EsraKrmn <107814202+EsraKrmn@users.noreply.github.com>
2022-07-18 19:09:47 +0300

    olduuu

--- Alt_Islemler/vdv3.ino
+++ Alt_Islemler/vdv3.ino
@@ -1,32 +1,33 @@
 float vin=0.0;   //battery voltage
 float vout=0.0;    //A0 pin reading voltage
-float r1=47200.0;    //r1 value
-float r2=66700.0;      //r2 value
+float r1=11880.0;    //r1 value
+float r2=10000.0;      //r2 value
 
 void setup()
-{
+{  
+   analogReadResolution(12);
    Serial.begin(9600);     //  opens serial port, sets data rate to 9600 bps
 }
 
 void loop()
 {
   float vtoplam= 0.0;
-  int count=1;
+  int count=0;
   
-  for(int i=0; i<30; i++)
+  for(int i=0; i<75; i++)
     {
-      float analogvalue = analogRead(A0);      //reading value from analog pin
-      vout = analogvalue * 5 / 1024 ;           //analog value to voltage (it is pin's voltage)
+      float analogvalue = analogRead(34);      //reading value from analog pin
+      vout = analogvalue * 2.373 / 4096 ;           //analog value to voltage (it is pin's voltage)
       vin = (((r1+r2)* vout)/r2);
-      //Serial.println(vin);
+      //Serial.println(vout);
       if(vin != 0.00)
       {
         vtoplam = vtoplam + vin;
         count = count+1 ;
       }
-      delay(25);
+      delay(10);
     }
-  float voltage = vtoplam/(count-1);
+  float voltage = 2.32 * (vtoplam/count);
   Serial.print("Voltage= ");        
   Serial.print(voltage);
   Serial.println(" Volts");
